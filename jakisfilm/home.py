e80281c27366bfc288bfb0ebb4ae6258ad0ad8a8
mLorenc <lorencmaciek@gmail.com>
2018-06-18 02:46:31 +0200

    Improved homepage and added next movie button to player

--- jakisfilm/home.py
+++ jakisfilm/home.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+from jakisfilm.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
@@ -7,5 +8,12 @@ bp = Blueprint('home', __name__, url_prefix='/')
 @bp.route('/')
 def home():
 
+    db = get_db()
+    movie_count = db.execute(
+        'SELECT COUNT(*)'
+        ' FROM movie'
+    ).fetchone()
+
     return render_template(
-        'home/home.html')
+        'home/home.html',
+        movie_count=movie_count[0])
